77a67f0700f49515760e338c7923097882af8b51
Sagar Bhatnagar <48132955+SagarBhatnagar@users.noreply.github.com>
2020-03-14 19:24:43 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 export TRAINING_DATA=input/train_folds.csv
-# export FOLD =
-
-FOLD=0 
+export FOLD = 0
+export MODEL = $1
+ 
 python -m src.train
 #FOLD=1 python -m src.train
 #FOLD=2 python -m src.train
